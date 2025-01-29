6987b9396c0bb5d4473b86a1a14d19c700a46a34
fardin <fardin.pvt.7@gmail.com>
2025-01-29 22:10:50 +0530

    Register Form Create

--- expense-tracker/src/components/Homepage.js
+++ expense-tracker/src/components/Homepage.js
@@ -2,19 +2,53 @@ import React from 'react'
 import img1 from '../asset/img/WhatsApp Image 2025-01-25 at 21.15.16_14405a77.jpg'
 function Homepage() {
   return (
-    <div className= 'container'>
-      <div className= 'leftcontainer'>
-          
-          <img src={img1} />
+    <div className='container'>
+      <div className='leftcontainer'>
+        <p>Welcome to Expense Tracker</p>
+        <p>Manage all your Expense</p>
+        <img src={img1} />
+      </div>
+      <div className='rightcontainer'>
+        <div className='formcard'>
+         <div> <h2 style={{alignSelf:"center"}}>Register</h2></div> 
+          <div>
+            <label>Name</label>
+            <input 
+            required
+            type="text" 
+            placeholder='Enter your Name'/>
           </div>
-      <div className= 'rightcontainer'> 
-         
-         </div>
-       
-      
+          <div>
+            <label>E-mail</label>
+            <input 
+            required
+            type="e-mail" 
+            placeholder='Enter your E-mail'/>
+          </div>
+          <div>
+            <label>Password</label>
+            <input 
+             required
+             type="password" 
+             placeholder='Enter your Password'/>
+          </div>
+          <div>
+            <label>Confirm Password</label>
+            <input 
+             required
+             type="password" 
+             placeholder='Enter your Password'/>
+          </div>
+          <div>
+          <button> Submit </button>
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
   )
 }
 
 export default Homepage
- 
